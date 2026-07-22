6930153f9199e899333f9abe28b3b5a92aca15d0
Bruno Diniz <brunodevdrums@gmail.com>
2026-07-22 10:37:10 +0100

    new commands: help, ls, clear, reset, randomNumber, open apps. css changes

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const commandInput = document.getElementById("commandInput");
 
 let commands = {
     "info": () => addLine("Made by Bruno in 07/22/26", "string"),
+    "ls": () => addLine("README.md index.html styles/ public/ backend/", "string"),
+    "help": () => help(),
+    "open youtube": () => openLink("https://www.youtube.com/"),
+    "open spotify": () => openLink("https://open.spotify.com/"),
+    "clear": () => clear(),
+    "reset": () => reset(),
+    "randomNumber": () => getNumber(),
 }
 
 function addLine(command, type) {
@@ -10,7 +17,7 @@ function addLine(command, type) {
     li.innerText = `${command}`;
     li.classList.add("command");
     
-    if (type === "string" || type === "int") {
+    if (type === "string" || type === "int" || type === "error") {
         li.classList.add(type)
     }
     else {
@@ -20,6 +27,35 @@ function addLine(command, type) {
     commandsDisplayer.appendChild(li);
 }
 
+function help() {
+    addLine("List of commands:", "string");
+    addLine("`info` : See the info of this terminal", "string");
+    addLine("`ls` : See what is in your current directory", "string");
+    addLine("`help` : get help with commands", "string");
+    addLine("`open youtube` : Open youtube on your broswer!", "string");
+    addLine("`open spotify` : Open Spotify on your broswer!", "string");
+    addLine("`clear` : It removes every line in your terminal", "string");
+    addLine("`reset` : It resets this page", "string");
+    addLine("`randomNumber` : Get a random number!", "string");
+}
+
+function openLink(link) {
+    window.location.href = link;
+}
+
+function clear() {
+    commandsDisplayer.innerHTML = "";
+}
+
+function reset() {
+    window.location.reload();
+}
+
+function getNumber() {
+    const number = Math.floor(Math.random() * 10000000);
+    addLine(`${number}`, "int");
+}
+
 function readInput() {
     let command = commandInput.value;
 
@@ -27,6 +63,9 @@ function readInput() {
 
     if (commands[command]) {
         commands[command]();
+    }
+    else {
+        addLine(`No command named '${command}' found :(`, "error")
     }
 
     commandInput.value = "";
